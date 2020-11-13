f2e52d705d015106fbd6290e25f8f5285f065ce0
Hector Acosta <hacostacervantes17@mail.wou.edu>
2020-11-13 15:43:15 -0800

    Added timer for temporary display of string in logic2.js file.

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -10,12 +10,20 @@ var round = 1;
 // Variable used to keep track or random number generated to remember and compare to inputed answer.
 var randomString = "";
 
+// Variable used to setup interval timer.
+var timer;
+
+// Variables used to set time.
+var maxCountDownTime = 3;
+var countDownTime = 3;
+
 $("form").submit(function (event) {
     // When going from directions to game, will set up a lot of the display items.
     if (currentState == GAMESTATE.DIRECTIONS) {
         $(".IntroContent").empty();
         $(".MainContent").append('<h5>Level:</h5>');
         $(".MainContent").append('<p id="round">'+round+'</p>')
+        $(".MainContent").append('<p id="timer"></p>');
         $(".MainContent").append('<hr>');
         $(".MainContent").append('<h1 id="displayString"></h1>');
         currentState = GAMESTATE.START;
@@ -35,11 +43,16 @@ $("form").submit(function (event) {
         $('#submit').remove();
     }
 
+
     // Setup random generated stirng part.
     SetupRandomNumber();
 
     // Set Timeout to erase displayed string and setup input.
-    setTimeout(function () { SetupInputSection(); }, 3000);
+    setTimeout(function () { SetupInputSection(); }, maxCountDownTime * 1000);
+
+    // Setup interval timer to display how long displayed string will stray on screen.
+    $('#timer').text(countDownTime);
+    timer = setInterval(TickDownTime,1000);
 
     event.preventDefault();
 
@@ -81,3 +94,17 @@ function generate_random_string(string_length) {
     return random_string
 }
 
+// Function used to lower countDownTime and display it unless time hits 0 in which case it resets it.
+function TickDownTime(){
+    countDownTime--;
+    // If countDownTime is more than 0 then display it.
+    if(countDownTime > 0){
+        $('#timer').text(countDownTime);
+    }
+    // Clear/Reset items relating to the timer.
+    else{
+        $('#timer').empty();
+        clearInterval(timer);
+        countDownTime = maxCountDownTime;
+    }
+}
