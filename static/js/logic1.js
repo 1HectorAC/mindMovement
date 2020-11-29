606b38e37196399dd9d4bad3e8081dd84f0013d6
Hector Acosta <hacostacervantes17@mail.wou.edu>
2020-11-28 16:35:18 -0800

    Refactored code in logic1.js file.

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -1,46 +1,110 @@
+// Manage state of game.
+const GAMESTATE = {
+    MENU: 0,
+    PLAY: 1,
+    GAMEOVER: 2
+}
+
+// Manage operation parts need for setting up the option. (element id, checkbox value, element label).
+const OPERATION_LIST = [
+    ["add", "+", "Addition (+)"],
+    ["minus","-"," Subtraction (-)"],
+    ["times","*","Multiplication (*)"]
+];
+
+var currentState = GAMESTATE.MENU;
+
+// Number of Questions in game.
+var numberOfQuestions = 5;
+
+// Max number possible for equation question.
+var MaxNumberOption = 9;
+
+// List containing answers to all questions in a game.
+var answerList = [];
+
+setupOption();
 
 $("form").submit(function (event) {
     var mainContent = $('#mainContent');
 
-    if (mainContent.children().length == 0) {
+    // If in the menu state then move one to starting game.
+    if (currentState == GAMESTATE.MENU) {
+        // Resut answerList incase playing again.
+        answerList = [];
+
         var ops = document.getElementsByName("operation");
         var operation = GetOperation(ops);
 
-        for (var i = 0; i < 5; i++) {
-            AddEquation(operation, mainContent, i, i);
+        for (var i = 0; i < numberOfQuestions; i++) {
+            AddEquation(operation, mainContent);
         }
 
         mainContent.append('<input id="submitAnswer" class="btn btn-primary customButton" type="submit" value="Done">');
 
         //clear the form
         $('#optionPart').empty();
-    } else if($('#submitRetry').length == 1){
+
+        currentState = GAMESTATE.PLAY;
+    }
+    // If Moving on after playing then move on the the gameOver screen.
+    else if(currentState == GAMESTATE.PLAY){
+        // Remove Submit answers button.
+        $('#submitAnswer').remove();
+
+        //make answer spots unchangable.
+
+        //calculate Results here
+        mainContent.append('<h1>Total Correct: '+GetAnswerTotal()+' out of '+ numberOfQuestions +'</h1>');
+        mainContent.append('<input id="submitRetry" class="btn btn-primary customButton" type="submit" value="Retry">');
+        currentState = GAMESTATE.GAMEOVER;
+    }
+    // If moving on after gameOver then setup replay.
+    else if(currentState == GAMEOVER){
         setupOption();
         mainContent.empty();
-    } else{
-        $('#submitAnswer').remove();
-        //make answer spots unchangable.
-        //calculate Results here
-        mainContent.append('<h1>Total Correct: '+GetAnswerTotal()+' out of '+$('.entryFieldClass').length+'</h1>');
-        mainContent.append('<input id="submitRetry" class="btn btn-primary customButton" type="submit" value="Retry">');
+        currentState = GAMESTATE.MENU;
     }
     event.preventDefault();
 
 });
 
-function GetAnswerTotal(){
-    var enteredValues = $('.entryFieldClass');
-    var answerValues = $('.answerFieldClass');
-    var total = 0;
-    for(i = 0; i < enteredValues.length; i++){
-        if(enteredValues[i].value == answerValues[i].value){
-            total++;
-        }
+//Setup up option selection part at start.
+function setupOption(){
+    var optionPart = $('#optionPart');
+    optionPart.append('<h5>~Enter an option~</h5>');
+    optionPart.append('<hr>')
+
+    // Setup operation checkbox options.
+    for(i = 0; i < OPERATION_LIST.length; i++){
+        // Add 'checked' for first checkbox option.
+        if(i==0)
+            optionPart.append('<input type="radio" id="'+OPERATION_LIST[i][0]+'" name="operation" value="'+OPERATION_LIST[i][1]+'" checked>');
+        else
+            optionPart.append('<input type="radio" id="'+OPERATION_LIST[i][0]+'" name="operation" value="'+OPERATION_LIST[i][1]+'">');
+        optionPart.append('<label for="'+OPERATION_LIST[i][0]+'">'+OPERATION_LIST[i][2]+'</label><br>');
     }
-    return total;
+
+    optionPart.append('<input type="submit" value="Submit" class="btn btn-primary customButton">');
 }
+
+// Return the operation value that is checked in a checkbox that is passed in.
+function GetOperation(opsList) {
+    // Loop through every value in list to find the one that is checked and return the value.
+    for (i = 0; i < opsList.length; i++) {
+        if (opsList[i].checked)
+            return opsList[i].value;
+    }
+    // If none is found then return 0.
+    return 0;
+}
+
+// Return answer of passed in operation applied to two passed in numbers.
 function GetOperationAnswer(one, two, operation) {
     switch (operation) {
+        case "+":
+            return one + two;
+            break;
         case "-":
             return one - two;
             break;
@@ -52,46 +116,41 @@ function GetOperationAnswer(one, two, operation) {
     }
 }
 
-function AddEquation(operation, htmlPlacement, enterFieldName, answerName) {
-    var one = Math.floor(Math.random() * 9);
-    var two = Math.floor(Math.random() * 9);
+// Add equation question to the passed in element "htmlPlacement".
+function AddEquation(operation, htmlPlacement) {
+    var one = Math.floor(Math.random() * MaxNumberOption);
+    var two = Math.floor(Math.random() * MaxNumberOption);
     var answer = GetOperationAnswer(one, two, operation);
 
-    //Add Equation.
+    // Add Equation.
     htmlPlacement.append('<h1 class="equationField">' + one + operation + two + '=</h1>');
 
-    //Add field to enter answer
-    htmlPlacement.append('<input name="' + enterFieldName + '" class="entryFieldClass" type="number" min="-99" max="99" required="" >');
+    // Add field to enter answer.
+    htmlPlacement.append('<input class="entryFieldClass" type="number" min="-99" max="99" required="" >');
 
-    //Add hidden answer field
-    htmlPlacement.append('<input type="hidden" id="stuff" class="answerFieldClass" name="' + answerName + '" value="' + answer + '">');
+    //Add answer to answerList.
+    answerList.push(answer);
+
     htmlPlacement.append("<hr>");
 
 }
 
-function GetOperation(opsList) {
-    //get array of operations.
-    var ops = document.getElementsByName("operation");
-
-    //Check which operation in list is checked and return it.
-    for (i = 0; i < ops.length; i++) {
-        if (ops[i].checked)
-            return ops[i].value;
+// Return total number of answers that match input and actual answer.
+function GetAnswerTotal(){
+    var enteredValues = $('.entryFieldClass');
+    var total = 0;
+    for(i = 0; i < enteredValues.length; i++){
+        if(enteredValues[i].value == answerList[i]){
+            total++;
+        }
     }
-    return 0;
+    return total;
 }
 
-function setupOption(){
-    var optionPart = $('#optionPart');
-    optionPart.append('<h5>~Enter an option~</h5>');
-    optionPart.append('<hr>')
-    optionPart.append('<input type="radio" id="add" name="operation" value="+" checked>');
-    optionPart.append('<label for="add"> Addition (+)</label><br>');
-    optionPart.append('<input type="radio" id="minus" name="operation" value="-">');
-    optionPart.append('<label for="minus"> Subtraction (-)</label><br>');
-    optionPart.append('<input type="radio" id="times" name="operation" value="*">');
-    optionPart.append('<label for="times"> Multiplication (*)</label><br>');
-    optionPart.append('<input type="submit" value="Submit" class="btn btn-primary customButton">');
-    
-}
-setupOption();
+
+
+
+
+
+
+
