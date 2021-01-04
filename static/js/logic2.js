92fb873bf1d992f27c813d59356f8f471a43b03f
Hector Acosta <hacostacervantes17@mail.wou.edu>
2021-01-03 16:12:23 -0800

    Refactored code in logic2.js file.

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -1,11 +1,3 @@
-// Manage state of game.
-const GAMESTATE = {
-    DIRECTIONS: 0,
-    START: 1,
-    LOAD: 2
-};
-var currentState = GAMESTATE.DIRECTIONS;
-
 // Variable to keep track of current round.
 var round = 1;
 var hightestRound = round;
@@ -23,111 +15,198 @@ var countDownTime = maxCountDownTime;
 var maxLives = 1;
 var lives = maxLives;
 
-DirectionsScreen();
+DirectionsScreen($(".MainContent"));
 
-$("form").submit(function (event) {
-
-    // When going from directions to game, will set up a lot of the display items.
-    if (currentState == GAMESTATE.DIRECTIONS) {
-        // Check if value to set difficulty is set. Will only work the first time you play since directions aren't shown again.
-        if(  typeof $('#difficulty').val() !== 'undefined'){
-            if(parseInt($('#difficulty').val()) == 1){
-                maxLives = 4;
-                lives = maxLives;
-                maxCountDownTime = 6;
-                countDownTime = maxCountDownTime;
-            }
-            else{
-                maxLives = 2;
-                lives = maxLives;
-                maxCountDownTime = 3;
-                countDownTime = maxCountDownTime;
-            }
-        }
+$("body").on('click', 'button', function(ev) {
+    ev.preventDefault()
+    if ($(this).attr("value") == "Play") {
+        var difficulty = $('#difficulty').val();
 
         $(".MainContent").empty();
+
+        if(difficulty == "Easy"){
+            maxLives = 4;
+            lives = maxLives;
+            maxCountDownTime = 6;
+            countDownTime = maxCountDownTime;
+        }
+        else if(difficulty == "Hard"){
+            maxLives = 2;
+            lives = maxLives;
+            maxCountDownTime = 3;
+            countDownTime = maxCountDownTime;
+        }
+
+        // Set Round. Important for restarting game.
+        round = 1;
+        hightestRound = round;
 
         var gameVars = $("<div id='gameVars' class='row'>");
         gameVars.append("<div class='col-md-2'>Lives:<br><p id='lives'>"+ lives + "</p></div>");
         gameVars.append("<div class='offset-md-8 col-md-2'>Level:<br><p id='round'>" + round + "</p></div>");
         $(".MainContent").append(gameVars);
+        $(".MainContent").append('<hr>');        
 
-        $(".MainContent").append('<hr>');
-        $(".MainContent").append('<p id="hitMessage" style="font-size:20px; margin-bottom: 2px"></p>');
-        $(".MainContent").append('<p id="reminder" style="margin-bottom: 2px"></p>');
-        $(".MainContent").append('<h1 id="displayString"></h1>');
-        
-        TempStringDisplaySetup();
-
-        currentState = GAMESTATE.START;
+        TempStringDisplaySetup($(".MainContent"));
     }
-    // When not in DIRECTION state then will just play game again forever.
-    else if (currentState  == GAMESTATE.START) {
-        if(randomString == $('#EnterChar').val()){
-            $("#round").css("color", "green");
-            $("#hitMessage").text("Correct!");
-            $("#hitMessage").css("color", "green");
-            $("#reminder").css("color", "green");
+    if ($(this).attr("value") == "SubmitAnswer") {
+        var enteredString = $('#EnterChar').val();
+
+        $(".inputSection").remove();
+
+        // Hit or miss check.
+        if(randomString == enteredString){
             round++;
             $('#round').text(round);
             if(round > hightestRound)
                 hightestRound = round;
+            
+            // Setup for another round.
+            TempStringDisplaySetup($(".MainContent"));
         }
         else{
             lives--;
-            $("#lives").text(lives);
+            // GameOver Check.
+            if(lives <= 0){
+                $(".MainContent").empty();
+                EndGameScreen($(".MainContent"));
 
-            $("#hitMessage").text("Miss");
-            $("#round").css("color", "red");
-            $("#hitMessage").css("color", "red");
-            $("#reminder").css("color", "red");
-            if(round > 1){
-                round--;
-                $('#round').text(round);
+                // Reset a few variables for if playing again.
+                lives = maxLives;
+                round = 1;
+                hightestRound = round;
             }
-            
-        }
+            else{
+                $("#lives").text(lives);
+    
+                if(round > 1){
+                    round--;
+                    $('#round').text(round);
+                }
 
-        // Game over check.
-        if(lives > 0){
-            currentState = GAMESTATE.LOAD;
-            var inputedChar = $('#EnterChar').val();
-            $('.inputSection').remove();
+                // Add results screen to compare answer with actual answer.
+                ResultsScreen($(".MainContent"),enteredString);
 
-            // Add results screen to compare answer with actual answer.
-            ResultsScreen(inputedChar);
-        }
-        else{
-            currentState = GAMESTATE.DIRECTIONS;
-            $(".MainContent").empty();
-            $(".MainContent").append(EndGameScreen());
-
-            // Reset a few variables for if playing again.
-            lives = maxLives;
-            hightestRound = 1;
-            round = 1;
-
-            
+            }
         }
     }
-    else if(currentState == GAMESTATE.LOAD){
+    if ($(this).attr("value") == "AfterLoss") {
         $('#resultItems').remove();
-        $('#hitMessage').text('');
-
-        TempStringDisplaySetup();
-        currentState = GAMESTATE.START;
-
-
+        TempStringDisplaySetup($(".MainContent"));
     }
-    else{
+    
+    if ($(this).attr("value") == "Instructions") {
         $(".MainContent").empty();
+        DirectionsScreen($(".MainContent"));
+    }
+});
+
+// Setup items for directions screen.
+function DirectionsScreen(element){
+    var screenItems = $("<div></div>");
+    screenItems.append("<h5>-Instructions-</h5>");
+    screenItems.append("<p>A Character string will appear and disapear in a few seconds. Rememeber the string and type it in the box that appears afterwards. Larger strings will be displayed after every correct answer. Wrong answers will make the strings shorter. The difficulty settings will affect how a long a string is displayed and amount of lives.</p>");
+    screenItems.append("<hr>");
+
+    // Setup dropdown for difficulty settings.
+    screenItems.append('<h5>-Difficulty-</h5>');
+    var selectSection = $('<select id="difficulty" class="btn btn-secondary"></select>');
+    selectSection.append('<option value="Easy"> Easy (6 seconds, 4 lives)</option>');
+    selectSection.append('<option value="Hard"> Hard (3 seconds, 2 lives)</option>');
+    screenItems.append(selectSection);
+    
+    screenItems.append("<hr>");
+    screenItems.append('<button type="submit" class="btn btn-primary customButton" value="Play">Play</button>');
+    element.append(screenItems);
+}
+
+// Setup screen for temporary string display. This includes timer to start next part.
+function TempStringDisplaySetup(element){
+    // Need to setup randomString for display. Will also be used later to compare with a answer of user.
+    randomString = SetupRandomString(round+ 2);
+    
+    var displayElements = $('<div id="tempDisplay"></div>')
+    displayElements.append('<p id="reminder" style="margin-bottom: 2px">Remember string below!</p>');
+    displayElements.append('<h1 id="displayString">'+ randomString +'</h1>');
+    displayElements.append('<p id="timer" class="roundButton">'+countDownTime+'</p>');
+    
+    element.append(displayElements);
+
+    // Setup interval timer to display how long displayed string will stray on screen.
+    timer = setInterval(CountDownAction,1000);
+}
+
+// Function used to lower countDownTime and display it unless time hits 0 in which case it resets it and takes end of time actions.
+function CountDownAction(){
+    countDownTime--;
+    // If countDownTime is more than 0 then display it.
+    if(countDownTime > 0){
+        $('#timer').text(countDownTime);
+    }
+    // Clear/Reset items relating to the timer and take end of time action.
+    else{
+        // Removes items like temp string, reminder, timer.
+        $('#tempDisplay').remove();
+        
+        clearInterval(timer);
+        countDownTime = maxCountDownTime;
+
+        // Added senction for input.
+        SetupInputSection($('.MainContent'));
+        
+    }
+}
+
+// Setup parts to accepts inputs in a textbox and a submit button.
+function SetupInputSection(element) {
+    //Setup items to add to display.
+    var inputSection = $('<div class="inputSection"/>');
+    inputSection.append('<input  class="form-control" id="EnterChar" style="width:50%;display: inline-block; font-size: 30px; text-align:center" placeholder="Enter string here!" maxlength="50">');
+    inputSection.append('</br>');
+    inputSection.append('<button type="submit" value="SubmitAnswer" id="submit" class="btn btn-primary customButton" style="Margin-top:10px">Enter</button>');
+    element.append(inputSection);
+}
+
+// Setup items for results screen.
+function ResultsScreen(element, inputedChar){
+    var resultItems = $('<div id="resultItems"></div>');
+    resultItems.append('<p id="hitMessage" style="font-size:20px; margin-bottom: 2px; color:red">Miss</p>');
+
+    resultItems.append('<h5>Answer: '+randomString+'</h5>');
+
+    var indexPoint = DifferenceIndex(randomString,inputedChar);
+
+    // Setup display of inputed string with correction.
+    if(indexPoint != -1){
+        var fHalf = inputedChar.substring(0,indexPoint);
+        var sHalf = inputedChar.substring(indexPoint, inputedChar.length);
+        resultItems.append('<h5>Your Answer: '+fHalf+'<span style="color:red">'+sHalf+'</span></h5>');
+    }
+    // Case where full string wasn't entered and need '_' to indicate missing chars.
+    else if(randomString.length > inputedChar.length){
+        var lengthDif = randomString.length - inputedChar.length;
+        resultItems.append('<h5>Your Answer: '+ inputedChar +'<span style="color:red">'+  "_".repeat(lengthDif) + '</span></h5>');
+    }
+    // Case where entered more chars then needed.
+    else{
+        var fHalf = inputedChar.substring(0,randomString.length);
+        var sHalf = inputedChar.substring(randomString.length, inputedChar.length);
+        resultItems.append('<h5>Your Answer: '+fHalf+'<span style="color:red">'+sHalf+'</span></h5>');
     }
 
+    resultItems.append('<button type="submit" value="AfterLoss" id="submit" class="btn btn-primary customButton" style="Margin-top:10px">Next</button>');
+    element.append(resultItems);
+}
 
+// Setup items for end game screen.
+function EndGameScreen(element){
+    var endGameItems = $('<div></div>');
+    endGameItems.append('<h1 style="color:red">Game Over</h1>');
+    endGameItems.append('<h5>Highest Round Reached: '+hightestRound+'</h5>');
+    endGameItems.append('<button type="submit" value="Instructions" class="btn btn-primary customButton" style="Margin-top:10px">Retry</button>');
 
-    event.preventDefault();
-
-});
+    element.append(endGameItems);
+}
 
 // Return a randomly generated string of a length based on passed value.
 // Note that the shortest string length is 3.
@@ -154,110 +233,6 @@ function generate_random_string(string_length) {
         random_string += String.fromCharCode(random_ascii);
     }
     return random_string
-}
-
-// Setup parts to accepts inputs in a textbox and a submit button.
-function SetupInputSection() {
-    //Setup items to add to display.
-    var inputSection = $('<div class="inputSection"/>');
-    inputSection.append('<input name="stuff" class="form-control" id="EnterChar" style="width:50%;display: inline-block; font-size: 30px; text-align:center" placeholder="Enter string here!">');
-    inputSection.append('</br>');
-    inputSection.append('<input type="submit" value="Submit" id="submit" class="btn btn-primary customButton" style="Margin-top:10px">');
-    return inputSection;    
-}
-
-// Function used to lower countDownTime and display it unless time hits 0 in which case it resets it and takes end of time actions.
-function CountDownAction(){
-    countDownTime--;
-    // If countDownTime is more than 0 then display it.
-    if(countDownTime > 0){
-        $('#timer').text(countDownTime);
-    }
-    // Clear/Reset items relating to the timer and take end of time action.
-    else{
-        $('#timer').remove();
-        clearInterval(timer);
-        countDownTime = maxCountDownTime;
-
-        // Clear a few items beforehand.
-        $('#reminder').text('');
-        $('#displayString').text('');
-
-        $('.MainContent').append(SetupInputSection());
-        
-    }
-}
-
-// Setup screen for temporary string display. This includes timer to start next part.
-function TempStringDisplaySetup(){
-    $(".MainContent").append('<p id="timer" class="roundButton"></p>');
-    $("#reminder").text("Remember string below!");
-
-    // Setup random generated string part.
-    randomString = SetupRandomString(round+ 2);
-    $('#displayString').text(randomString);
-
-    // Setup interval timer to display how long displayed string will stray on screen.
-    $('#timer').text(countDownTime);
-    timer = setInterval(CountDownAction,1000);
-}
-
-// Setup items for end game screen.
-function EndGameScreen(){
-    var endGameItems = $('<div></div>');
-    endGameItems.append('<h1 style="color:red">Game Over</h1>');
-    endGameItems.append('<h5>Highest Round Reached: '+hightestRound+'</h5>');
-    endGameItems.append('<input type="submit" value="Retry" class="btn btn-primary customButton" style="Margin-top:10px">');
-
-    return endGameItems;
-}
-
-// Setup items for directions screen.
-function DirectionsScreen(){
-    var screenItems = $("<div></div>");
-    screenItems.append("<h5>-Instructions-</h5>");
-    screenItems.append("<p>A Character string will appear and disapear in a few seconds. Rememeber the string and type it in the box that appears afterwards. Larger strings will be displayed after every correct answer. Wrong answers will make the strings shorter. The difficulty settings will affect how a long a string is displayed and amount of lives.</p>");
-    screenItems.append("<hr>");
-
-    // Setup dropdown for difficulty settings.
-    screenItems.append('<h5>-Difficulty-</h5>');
-    var selectSection = $('<select id="difficulty" class="btn btn-secondary"></select>');
-    selectSection.append('<option value="1"> Easy (6 seconds, 4 lives)</option>');
-    selectSection.append('<option value="2"> Hard (3 seconds, 2 lives)</option>');
-    screenItems.append(selectSection);
-    
-    screenItems.append("<hr>");
-    screenItems.append('<input type="submit" class="btn btn-primary customButton" value="Play">');
-    $(".MainContent").append(screenItems);
-}
-
-// Setup items for results screen.
-function ResultsScreen(inputedChar){
-    var resultItems = $('<div id="resultItems"></div>');
-    resultItems.append('<h5>Answer: '+randomString+'</h5>');
-
-    var indexPoint = DifferenceIndex(randomString,inputedChar);
-
-    // Setup display of inputed string with correction
-    if(indexPoint != -1){
-        var fHalf = inputedChar.substring(0,indexPoint);
-        var sHalf = inputedChar.substring(indexPoint, inputedChar.length);
-        resultItems.append('<h5>Your Answer: '+fHalf+'<span style="color:red">'+sHalf+'</span></h5>');
-    }
-    // Case where full string wasn't entered and need '_' to indicate missing chars.
-    else if(randomString.length > inputedChar.length){
-        var lengthDif = randomString.length - inputedChar.length;
-        resultItems.append('<h5>Your Answer: '+ inputedChar +'<span style="color:red">'+  "_".repeat(lengthDif) + '</span></h5>');
-    }
-    // Case where entered more chars then needed.
-    else{
-        var fHalf = inputedChar.substring(0,randomString.length);
-        var sHalf = inputedChar.substring(randomString.length, inputedChar.length);
-        resultItems.append('<h5>Your Answer: '+fHalf+'<span style="color:red">'+sHalf+'</span></h5>');
-    }
-
-    resultItems.append('<input type="submit" value="Next" id="submit" class="btn btn-primary customButton" style="Margin-top:10px">');
-    $(".MainContent").append(resultItems);
 }
 
 // Compare two strings and return the index point at which they match up the the length of the shortest lenght.
